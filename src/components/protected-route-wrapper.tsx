e3187c344731b10ad34e2db1f42037b4be7ad297
ezerssss <magbanuaezra@gmail.com>
2025-11-07 22:41:18 +0800

    auth bypass onboarding

--- src/components/protected-route-wrapper.tsx
+++ src/components/protected-route-wrapper.tsx
@@ -5,9 +5,11 @@ import { ReactNode, useEffect, useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
 
 import { onAuthStateChanged } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
 import { PuffLoader } from "react-spinners";
 
 import UnauthorizedPage from "@/app/unauthorized/page";
+import { USERS_COLLECTION_REF } from "@/constants/firebase";
 import { auth } from "@/firebase";
 
 interface PropsInterface {
@@ -29,6 +31,22 @@ function ProtectedRouteWrapper(props: PropsInterface): JSX.Element {
 
       if (!user) {
         router.push(`/auth/login?backTo=${pathname}`);
+        setIsLoading(false);
+        return;
+      }
+
+      const userDocRef = doc(USERS_COLLECTION_REF, user.uid);
+      const userDoc = await getDoc(userDocRef);
+
+      if (!userDoc.exists()) {
+        router.push("/onboarding");
+        return;
+      }
+
+      const userData = userDoc.data();
+
+      if (!userData.isOnboarded) {
+        router.push("/onboarding");
       }
 
       setIsLoading(false);
